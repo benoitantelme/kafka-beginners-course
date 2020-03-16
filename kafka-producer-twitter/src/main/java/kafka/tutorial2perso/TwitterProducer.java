d7eede21e513c56dedd470ccbcb329024c9ab7b0
benoitantelme <benoitantelme@gmail.com>
2020-03-16 14:51:22 +0000

    Setting up hosebird client and kafka producer start and stop

--- kafka-producer-twitter/src/main/java/kafka/tutorial2perso/TwitterProducer.java
+++ kafka-producer-twitter/src/main/java/kafka/tutorial2perso/TwitterProducer.java
@@ -1,5 +1,14 @@
 package kafka.tutorial2perso;
 
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -9,7 +18,10 @@ import org.slf4j.LoggerFactory;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class TwitterProducer {
     Logger LOGGER = LoggerFactory.getLogger(TwitterProducer.class.getName());
@@ -58,11 +70,36 @@ public class TwitterProducer {
         return new KafkaProducer(prop);
     }
 
+    public Client createClient(){
+        // Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        hosebirdEndpoint.trackTerms(List.of("kafka"));
+
+        Authentication hosebirdAuth = new OAuth1(apiKey, apiSecretKey, accessToken, accessTokenSecret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        return builder.build();
+    }
+
     public static void main(String[] args){
         TwitterProducer tp = new TwitterProducer();
+        Client hosebirdClient = tp.createClient();
+        KafkaProducer<String, String> producer = tp.createProducer();
 
-        System.out.println("a");
+        System.out.println(hosebirdClient);
+        System.out.println(producer);
 
+        hosebirdClient.stop();
+        producer.close();
     }
 
 
