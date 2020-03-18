45d5ab15e02af037c85b86ff57a95e68d44e7e42
benoitantelme <benoitantelme@gmail.com>
2020-03-18 10:50:56 +0000

    Setup of client with properties and simple test

--- kafka-consumer-elasticsearch/src/main/java/com.github.simplesteph.kafka/tutorial3perso/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com.github.simplesteph.kafka/tutorial3perso/ElasticSearchConsumer.java
@@ -6,18 +6,46 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class ElasticSearchConsumer {
+    static final Logger LOGGER = LoggerFactory.getLogger(ElasticSearchConsumer.class);
 
-    public static RestHighLevelClient createClient(){
-        String hostname = ""; // localhost or bonsai url
-        String username = ""; // needed only for bonsai
-        String password = ""; // needed only for bonsai
+    private String hostname;
+    private String username;
+    private String password;
 
-        // credentials provider help supply username and password
+    public ElasticSearchConsumer() throws IOException {
+        Properties prop = new Properties();
+        String propFileName = "elasticsearch.bonsai.info.properties";
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+        if (inputStream != null) {
+            prop.load(inputStream);
+        } else {
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        }
+
+        this.hostname = prop.getProperty("hostname");
+        this.username = prop.getProperty("username");
+        this.password = prop.getProperty("password");
+    }
+
+    public RestHighLevelClient createClient(){
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY,
                 new UsernamePasswordCredentials(username, password));
@@ -35,10 +63,21 @@ public class ElasticSearchConsumer {
         return client;
     }
 
-    public static void main(String[] args){
-        System.out.println("aa");
+    public static void main(String[] args) throws IOException {
+        ElasticSearchConsumer esc = new ElasticSearchConsumer();
+        RestHighLevelClient client = esc.createClient();
+
+        String jsonstring = "{ \" foo \": \" bar \" }";
+
+        IndexRequest request = new IndexRequest("twitter", "tweets")
+                .source(jsonstring, XContentType.JSON);
+
+        IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+        String id = response.getId();
+        LOGGER.info(id);
 
 
+        client.close();
     }
 
 }
